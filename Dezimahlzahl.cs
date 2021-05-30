0263c9b9234d273b0221d08b0306181a2bd8a058
cofie27 <78723197+cofie27@users.noreply.github.com>
2021-05-30 19:31:17 +0200

    Projektdateien hinzufügen.

---
+++ Dezimahlzahl.cs
@@ -0,0 +1,287 @@
+﻿//------------------------------------------------------------------------------------------------------------------------------------
+//Dateiname	    : Auf_ParameterURückgabewerte
+//Projekt	    : Auf_ParameterURückgabewerte
+//Beschreibung  :
+//Autoren	    : Paul A.
+//Erstellung	: 21.01.2021
+//letzte Änd.	: 25.01.2021
+//Fehler	    : 
+//Dokument	    : auf_Parameter & auf_Rückgabewerte
+//------------------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmen
+{
+    class Dezimalzahl
+    {
+        //Deklaration und Kapselung des Attributs
+        private double wert;
+
+        //Standardkonstruktor
+        public Dezimalzahl()
+        {
+            wert = 1.0;
+        }
+        //zweiter Konstruktor mit dem Attribut Anfangswert
+        public Dezimalzahl(double Anfangswert)
+            //das Attribut wert wird auf den Anfangswert gesetzt
+        {
+            wert = Anfangswert;
+        }
+        //Eingabemethode für wert
+        public void Eingabe()
+        {
+            //Eingabeaufforderung (alt)
+            Console.Write("Bitte eine Dezimalzahl eingeben: ");
+            wert = Convert.ToDouble(Console.ReadLine());
+            while (wert < 0)
+            {
+                Console.WriteLine("Bitte eine positive Dezimalzahl eingeben: ");
+                wert = Convert.ToDouble(Console.ReadLine());                                //Speichern der Benutzereingabe, wenn Eingabe > 0
+
+            }
+        }
+        public void setWert(double newWert)
+        {
+            //Eingabeaufforderung NEU -> wert wird auf newWert gesetzt
+                wert = newWert;
+        }
+
+        //Verschiedene Ausgabemethoden
+
+        //Rückgabe des Werts
+        public double getWert()
+        {
+            return (wert);
+        }
+        // Wert wird zum String konvertiert
+        override public string ToString()
+        {
+            string ausgabe = "";
+            ausgabe = Convert.ToString(wert);
+            return (ausgabe);
+        }
+        //Wert wird zurückgegeben
+        public double To15()
+        {
+            return (wert);
+        }
+        //Wert wird zurückgegeben
+        public double ToExp()
+        {
+            return (wert);
+        }
+        //Wert wird als int zurückgegeben
+        public long ToHex()
+        {
+            return Convert.ToInt64(wert);
+        }
+        //Wert wird im oktalen System zurückgegeben
+        public String ToOKT()
+        {
+            //Deklaration und Initialisierung von xDez/xOkt
+            ulong xDez = Convert.ToUInt64(wert);
+            string xOkt = "";
+
+            do
+            {
+                //privates Attrivut zwischenspeicher deklariert um den Rest dort zu speichern
+                string zwischenspeicher = Convert.ToString(xDez % 8);
+                //dem String xOkt wird ein Stelle hinzugefügt
+                xOkt = zwischenspeicher + xOkt;
+                //xDez wird durch 8 geteilt um den Wert oktal darstellen zu können
+                xDez = xDez / 8;                                                                
+
+            }
+            //solange xDez >=1, weil 0/8= 0 Rest 0 ->sonst endlos
+            while (xDez >= 1); 
+            //Rückgabe des Attributs xOkt
+            return (xOkt);                                       
+
+        }
+        //Ausgabe im binären System
+        public string ToBin()
+        {
+            //Deklaration und Initialisierung von xDez/xBin
+            ulong xDez = Convert.ToUInt64(wert);
+            string xBin = "";
+
+            do
+            {
+                //wenn der Rest von xDez/2 ungelich 0 ist, dann....
+                if (xDez % 2 != 0)                                                              
+                {
+                    //Dem String xBin wird die "1" hinzugefügt
+                    xBin = "1" + xBin;                                                         
+                }
+                else
+                {
+                    //Dem String xBin wird die "0" hinzugefügt
+                    xBin = "0" + xBin;                                                          
+                }
+                //xDez/2 da man so die Dezimalzahl binär darstellen kann
+                xDez = xDez / 2;                                                                
+
+            }
+            //solange xDez >=1, weil 0/2= 0 Rest 0 ->sonst endlos (Abbruchbedinung)
+            while (xDez >= 1);
+            //xBin wird zurückgegeben
+            return (xBin);                                                                       
+
+        }
+        //Ausgabe ganzzahliger Quadratwuurzel (, falls vorhanden)
+        public int WurzelGanz()
+        {
+            //Deklaration und Initialisierung von i
+            int i = 1;
+
+            //solange Eingabewert > i*i, dann... (Abbruchbedingung)
+            while (wert > i * i)                                                                
+            {
+                //i um 1 erhöhen
+                i++;                                                                            
+            }
+            //wenn Eingabewert gleich i*i, dann...
+            if (wert == i * i)                                                                  
+            {
+                //i wird zurückgegeben (wenn vorhanden), was in der if- Bedinung geprüft wird
+                return (i);                                                                      
+            }
+            //andernfalls, also wenn i*i nie gleich dem Eingabewert wird, dann wird -1 zurückgegeben
+            else
+            {
+                return (-1);                                                                    
+            }
+
+        }
+        // Ausgabe der Quadratwurzel durchs Heron-Verfahren
+        public double Heron()
+        {
+            //Deklaration und Initialisierung von x und y
+
+            //als float, um die Dezimalstellen (hier:Nachkommastellen) zu begrenzen und keine Endlosschleife zu erhalten
+            float y = 1;                                                                        
+            double x = wert;
+
+            //Abbruchbedingung
+            while (x > y)                                                                       
+            {
+                //x wird gleich dem Ergebnis von (x+y)/2 gesetzt
+                x = (x + y) / 2;                                                                
+                y = Convert.ToSingle(wert / x);
+            }
+            //Rückgabe der Quadratwurzel (x)
+            return (x);                                                                          
+        }
+        //Ausgabe des größten gemeinsamen Teilers, nach Eingabe von zwei Werten
+        public int ToGGT(int op2)
+        {
+            //Deklaration und Initialisierung von A, B, T und G
+            int A = 0;
+            int B = 0;
+            double T = 2;
+            double G = 1;
+
+            //A wird gleich dem anfangs eingegenen Wert gesetzt und zum int konvertiert
+            A = Convert.ToInt32(wert);     
+            //B wird gleich dem zweiten operanden gesetzt, also einer zweioten beliebigen Zahl
+            B = op2;                                                            
+            do
+            {
+                //wenn der Rest von A/T gleich null ist, dann...
+                if ((A % T == 0))                                                                                
+                {
+                    //wenn der Rest von A/B gleich null ist, dann...
+                    if (B % T == 0)                                                                               
+                    {
+                        //setze G gleich T
+                        G = T;                                                                                   
+                    }
+                }
+                //T um 1 erhöhen
+                T++;                     
+            }
+            //Abbruchbedingung
+            while (T <= A && T <= B);                     
+            //Rückgabe des gemeinsamen Teilers "G"
+            return (Convert.ToInt32(G));                                                                                          
+        }
+        //Methode, die errechnet, ob der anfangs eingegebene Wert eine Primzahl ist
+        public bool IstPrim()
+        {
+            //Initialieren und Deklarieren der Attribute
+            long x = Convert.ToInt64(wert);
+            long p = 3;
+            bool Primzahl = false;
+
+            //Bedingung, um Laufzeit zu verkürzen
+            if (x % 2 == 0)
+            {
+                Primzahl = false;
+            }
+            else
+            {
+
+                while (Primzahl == false)
+                {
+                    //um die Laufzeit weiter zu verkürzern, wird geguckt ,ob p größer gleich der Wurzel lt. Heron Verfahren
+                    if (p >= Heron())
+                    {
+                        Primzahl = true;
+                        return Primzahl;
+                    }
+                    else if (x % p == 0)
+                    {
+                        Primzahl = false;
+                        return Primzahl;
+                    }
+                    p = p+2;
+                }
+            }
+            //Bool wird zurückgegeben
+            return (Primzahl);
+        }
+        //Methode, die die Differenz des anfangs eingegebenen und beliebigen zweiten Wert errechnet
+        public int Differenz(int op3)
+        {
+            //C wird initialisiert und deklariert
+            int C = 0;
+            //D wird initialisiert und deklariert
+            int D = 0;
+            //C wird gleich op3 gesetzt
+            C = op3;
+            //D wird gleich dem Ergebnis der Rechung zweiter beliebiger Wert - anfangs eingegebener Wert gesetzt
+            D = (op3 - Convert.ToInt32(wert));
+            //solange D kleiner als 0, dann...
+            while (D < 0)
+            {
+                //rechne D * (-1)
+                D = (D*(-1));
+            }
+            return (D);
+        }
+        public bool istWert(int op4)
+        { 
+            //Wahrheitswert "Gleichheit" wird auf falsch gesetzt
+            bool Gleichheit = false;
+            //wenn der anfangs eingegebene Wert gleich dem Operanden 4 also, dem zweiten beliebigen Wert, den man eingegeben hat, dann...
+            if (wert == op4)
+            {
+                //Wahrheitswert wird auf "true" gesetzt.
+                Gleichheit = true;
+            }
+            else
+            {
+                //Wahrheitswert wird auf false gesetzt"
+                Gleichheit = false;
+            }
+            //Rückgabe des Wahrheitswertes
+            return (Gleichheit);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿//------------------------------------------------------------------------------------------------------------------------------------
+//Dateiname	    : Auf_ParameterURückgabewerte
+//Projekt	    : Auf_ParameterURückgabewerte
+//Beschreibung  :
+//Autoren	    : Paul Atuahene
+//Erstellung	: 21.01.2021
+//letzte Änd.	: 25.01.2021
+//Fehler	    : 
+//Dokument	    : auf_Parameter & auf_Rückgabewerte
+//------------------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Objekt "d1" der Klasse Dezimalahl erzeugt
+            Dezimalzahl d1 = new Dezimalzahl();             
+
+            //Menü
+            //Solange diese schleife wahr ist--> immer, dann...
+            Console.Write("Bitte einen Wert eingeben: ");
+            d1.setWert(Convert.ToInt32(Console.ReadLine()));
+            while (d1.getWert() < 0)
+            {
+                Console.WriteLine("Bitte geben Sie ein POSITIVE Dezimahl ein:");
+                d1.setWert(Convert.ToDouble(Console.ReadLine()));
+            }
+            while (true) 
+           {
+                //Auswahlanweisung, die einen einzelnen switch-Abschnitt zum Ausführen aus einer Liste von Kandidaten auswählt, die auf einem Mustertreffer mit dem Vergleichsausdruck basiert
+                switch (menue()) //Methode menue wird hier aufgerufen. dh. hier: das Menü Overlay wird ausgegeben
+                {
+                    //Fall 1: die Methode setWert setzt das Attribut "Wert" auf die Eingabe der Konsole
+                    case 1:
+                        d1.setWert(Convert.ToInt32(Console.ReadLine()));
+                        //solange dieser Wert kleiner als 0 ist wird der Anwender aufgefordert eine POSITIVE Zahl einzugeben
+                        while (d1.getWert() < 0) 
+                        {
+                            Console.WriteLine("Bitte geben Sie ein POSITIVE Dezimahl ein:");
+                            d1.setWert(Convert.ToDouble(Console.ReadLine()));
+                        }
+                        break;
+                    //Fall 2: Die Methode getWert wird genutzt, um den eingegebenen Wert nochmals auszugeben
+                    case 2:
+                        Console.WriteLine("Wert: "+d1.getWert());
+                        break;
+                    //Fall 3: Der anfangs eingegebene Wert wird mit 15 Nachkommastellen ausgegeben, indem an den Rückgabewert der Methode getWert 15 Nachkommastellen angehangen werden
+                    case 3:
+                        Console.WriteLine(d1.getWert());
+                        Console.WriteLine("Wert mit 15 Nachkommastellen: " + "{0:F15}", d1.getWert());
+                        break;
+                    //Fall 4: Der anfangs eingegebene Wert wird in wissenschaftlicher Schreibweise ausgegeben, indem der Rückgabewert der Methode ToExp ausgegeben wird
+                    case 4:
+                        Console.WriteLine(d1.getWert());
+                        Console.WriteLine("Wissenschaftliche Schreibweise: " + "{0:E}", d1.ToExp());
+                        break;
+                    //Fall 5: Der anfangs eingegebene Wert wird in hexadezimaler Darstellung ausgegeben, indem der Rückgabewert der Methode ToHex ausgegeben wird und zusätzlich dem Compiler gesagt wird, dass er in hexadezimaler Schreibweise ausgeben soll
+                    case 5:
+                        Console.WriteLine(d1.getWert());
+                        Console.WriteLine("Hexadezimal: "+ "{0:X}", d1.ToHex());
+                        break;
+                    //Fall 6: Der anfangs eingegebene Wert wird in binärer Darstellung ausgegeben, indem der Rückgabewert der Methode ToBin ausgegeben wird
+                    case 6:
+                        Console.WriteLine(d1.getWert());
+                        Console.WriteLine("Binär: " + d1.ToBin());
+                        break;
+                    //Fall 7: Der anfangs eingegebene Wert wird in oktaler Darstellung ausgegeben, indem der Rückgabewert der Methode ToOkt ausgegeben wird
+                    case 7:
+                        Console.WriteLine(d1.getWert());
+                        Console.WriteLine("Oktal: "+ d1.ToOKT());
+                        break;
+                    //Fall 8: Die Wurzel des anfangs eingegebnen Wertes wird mit Hilfe des Heronverfahrens ausgegeben, indem der Rückgabewert der Methdoe Heron ausgegeben wird
+                    case 8:
+                        Console.WriteLine(d1.getWert());
+                        Console.WriteLine("Wurzel lt Heron: "+d1.Heron());
+                        break;
+                    //Fall 9: Die Wurzel des anfangs eingegebnen Wertes wird ausgegeben, indem der Rückgabewert der Methdoe WurzelGanz ausgegeben wird
+                    case 9:
+                        Console.WriteLine(d1.getWert());
+                        Console.WriteLine("Ganzzahlige Wurzel: " + d1.WurzelGanz());
+                        break;
+                    //Fall 10: Es wird mit Hilfe der Methode IstPrim geprüft, ob der eingegebene Wert eine Primzahl ist. Die Methode istPrim gibt bei Primzahl "true" und bei keiner Primzahl "false" zurück. Dieser Rückgabewert wird dann auf der Konsole ausgegeben
+                    case 10:
+                        Console.WriteLine(d1.getWert());
+                        Console.WriteLine("Primzahl? " +d1.IstPrim());
+                        break;
+                    //Fall 11: Die Differenz des anfangs eingegebenen Wertes und eines weiteren beliebigen Wertes wird mit Hilfe der Methode Differenz berechnet. Der Wert den diese Methode zurück gibt wird hier auf der Konsole ausgegeben
+                    case 11:
+                        Console.WriteLine(d1.getWert());
+                        Console.Write("Geben Sie einen zweiten Wert ein: ");
+                        Console.WriteLine("Differenz der beiden Eingaben: " + d1.Differenz(Convert.ToInt32(Console.ReadLine())));
+                        break;
+                    //Fall 12: Mit der Methode istWert wird geprüft, ob der anfangs eingegebene Wert gleich einem beliebigen Zweiten Wert ist. Der Rückgabewert "true" -> ist gleich oder "false" ist ungleich wird hier auf der Konsole ausgegeben
+                    case 12:
+                        Console.WriteLine(d1.getWert());
+                        Console.Write("Geben Sie einen zweiten Wert ein: ");
+                        Console.WriteLine("Sind die Eingaben gleich?" + d1.istWert(Convert.ToInt32(Console.ReadLine())));
+                        break;
+                    //Fall 13: Mit der Methode toGGT wird der größte gemeinsame Teiler des anfangs eingegebenen Wertes und einen beliebigen zweiten Wertes ermittelt. Der Rückgabewert dieser Methode wird hier ausgegeben
+                    case 13:
+                        Console.WriteLine(d1.getWert());
+                        Console.Write("Geben Sie einen zweiten Wert ein: ");
+                        Console.WriteLine("Größter gemeinsamer Teiler: "+d1.ToGGT(Convert.ToInt32(Console.ReadLine())));
+                        break;
+                    //Fall 14: Hier wird das Programm beendet. Mit einem weiteren Tastendruck (Enter) in der Konsole, wird diese dann geschlossen
+                    case 14:
+                        System.Environment.Exit(1);
+                        break;
+                    //wenn keiner dieser 14 Fälle angesprochen wird, dh wenn zum Beispiel eine Zahl eingegeben wird die außerhalb des Bereichs 1-14 eingegeben wird, wird der Anwender explizit aufgefordert eine Zahl zwischen 1 und 14 einzugeben
+                    default:
+                        Console.WriteLine("Eingabe bitte zuwischen 1 und 14");
+                        break;
+                }
+            }
+        }
+        //Methode, die das Menü auf der Console ausgibt
+        public static int menue()
+        {
+            //Menü Overlay wird auf der Konsole ausgegeben
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("1) Wert eingeben");
+            Console.WriteLine("2) Wert ausgeben");
+            Console.WriteLine("3) Wert mit 15 Nachkommastellen ausgeben");
+            Console.WriteLine("4) In wissenschaftlicher Schreibweise ausgeben");
+            Console.WriteLine("5) Hexadezimal ausgeben");
+            Console.WriteLine("6) Binär ausgeben");
+            Console.WriteLine("7) Oktal ausgeben");
+            Console.WriteLine("8) Wurzel lt. Heron ausgeben");
+            Console.WriteLine("9) Ganzzahlige Wurzel ausgeben");
+            Console.WriteLine("10) Primzahl?");
+            Console.WriteLine("11) Differenz");
+            Console.WriteLine("12) Wert verlgeichen");
+            Console.WriteLine("13) Größter gemeinsamer Teiler:");
+            Console.WriteLine("14) Programm beenden");
+            Console.WriteLine("------------------------------------------------------");
+            Console.Write("Menue-Auswahl: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            return (i);
+            
+        }
+    }
+
+}
